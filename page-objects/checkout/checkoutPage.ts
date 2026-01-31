03a180d27ecb502108c9a1925c2028c9ba2d2299
danailzahariev <d.zahariev@icloud.com>
2026-01-31 18:05:10 +0200

    added checkoutPage.ts and checkout.spec.ts tests

--- page-objects/checkout/checkoutPage.ts
+++ page-objects/checkout/checkoutPage.ts
@@ -3,15 +3,69 @@ import {Locator, Page} from "@playwright/test";
 export class CheckoutPage {
 
     private readonly page: Page;
-    private readonly firstnameInput: Locator
-    private readonly lastnameInput: Locator
-    private readonly zipCode: Locator
+    private readonly firstnameInput: Locator;
+    private readonly lastnameInput: Locator;
+    private readonly zipCode: Locator;
+    private readonly errorLocator: Locator;
+    private readonly continueBtn: Locator
+    private readonly cancelBtn: Locator;
+    private readonly summaryInfo: Locator;
+    private readonly productInfo: Locator;
+    private readonly finishBtn: Locator;
+    private readonly completeHeader: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.firstnameInput = page.locator('[data-test="firstName"]');
         this.lastnameInput = page.locator('[data-test="lastName"]');
         this.zipCode = page.locator('[data-test="postalCode"]');
+        this.errorLocator = page.locator('[data-test="error"]');
+        this.continueBtn = page.locator('[data-test="continue"]');
+        this.cancelBtn = page.locator('[data-test="cancel"]');
+        this.summaryInfo = page.locator('.summary_info');
+        this.finishBtn = page.locator('[data-test="finish"]');
+        this.completeHeader = page.locator('[data-test="complete-header"]');
+        this.productInfo = page.locator('[data-test="inventory-item"]');
+    }
+
+    async fillShippingInfo(firstName: string, lastName: string, zipCode: string): Promise<void> {
+        await this.firstnameInput.fill(firstName);
+        await this.lastnameInput.fill(lastName);
+        await this.zipCode.fill(zipCode);
+    }
+
+    async clickFinish() {
+        await this.finishBtn.click();
+    }
+
+    async clickContinue() {
+        await this.continueBtn.click();
+    }
+
+    async clickCancel() {
+        await this.cancelBtn.click();
+    }
+
+    getCompleteHeader(): Locator {
+        return this.completeHeader;
+    }
+
+    getSummaryInfo(): Locator {
+        return this.summaryInfo;
+    }
+
+    async getProductPriceInfo(productName: string) {
+        return await this.productInfo.filter({hasText: productName})
+            .locator('[data-test="inventory-item-price"]').innerText();
+    }
+
+    async getProductNameInfo(productName: string) {
+        return await this.productInfo.filter({hasText: productName})
+            .locator('[data-test="inventory-item-name"]').innerText();
+    }
+
+    getErrorMessage() {
+        return this.errorLocator;
     }
 
     getFirstnameInput(): Locator {
---
+++ tests/checkout.spec.ts
@@ -0,0 +1,107 @@
+import {expect, test} from "@playwright/test";
+import {PageManager} from "../page-objects/pageManager";
+import products from '../test-data/products.json';
+import users from '../test-data/users.json';
+
+
+test.describe("Checkout - Saucedemo", async () => {
+    let pm: PageManager;
+    const PRODUCT_ONE = products.productOne;
+
+    test.beforeEach(async ({page}) => {
+        pm = new PageManager(page);
+
+        await pm.onLoginPage().goto();
+        await pm.onLoginPage().login(users.validUser.username, users.validUser.password);
+        await expect(page).toHaveURL(/.*inventory.html/);
+    });
+
+    test("Complete checkout with valid data", async ({page}) => {
+        const inventoryPage = pm.onInventoryPage();
+        const cartPage = pm.onCartPage();
+        const checkoutPage = pm.onCheckoutPage();
+
+        await inventoryPage.addProductToCart(PRODUCT_ONE.name);
+        await inventoryPage.openCart();
+        await cartPage.clickCheckout();
+
+        await expect(page).toHaveURL(/.*checkout-step-one.html/);
+        await checkoutPage.fillShippingInfo("Ivan", "Ivanov", "1000");
+        await checkoutPage.clickContinue();
+
+        await expect(page).toHaveURL(/.*checkout-step-two.html/);
+        await expect(checkoutPage.getSummaryInfo()).toBeVisible();
+        const itemPrice = await checkoutPage.getProductPriceInfo(PRODUCT_ONE.name);
+
+        expect(await checkoutPage.getProductNameInfo(PRODUCT_ONE.name)).toEqual(PRODUCT_ONE.name);
+        expect(itemPrice).toEqual(products.productOne.price);
+
+        await checkoutPage.clickFinish();
+
+        await expect(page).toHaveURL(/.*checkout-complete.html/);
+        await expect(checkoutPage.getCompleteHeader()).toHaveText('Thank you for your order!');
+
+    });
+
+    test("Validate: first name is required", async () => {
+        const inventoryPage = pm.onInventoryPage();
+        const cartPage = pm.onCartPage();
+        const checkoutPage = pm.onCheckoutPage();
+
+        await inventoryPage.addProductToCart(PRODUCT_ONE.name);
+        await inventoryPage.openCart();
+        await cartPage.clickCheckout();
+
+        await checkoutPage.fillShippingInfo("", "Ivanov", "1000");
+        await checkoutPage.clickContinue();
+
+        await expect(checkoutPage.getErrorMessage()).toContainText('First Name is required');
+    });
+
+    test("Validate: last name is required", async () => {
+        const inventoryPage = pm.onInventoryPage();
+        const cartPage = pm.onCartPage();
+        const checkoutPage = pm.onCheckoutPage();
+
+        await inventoryPage.addProductToCart(PRODUCT_ONE.name);
+        await inventoryPage.openCart();
+        await cartPage.clickCheckout();
+
+        await checkoutPage.fillShippingInfo("Ivan", "", "1000");
+        await checkoutPage.clickContinue();
+
+        await expect(checkoutPage.getErrorMessage()).toContainText('Last Name is required');
+    });
+
+
+    test("Validate: zip-code is required", async () => {
+        const inventoryPage = pm.onInventoryPage();
+        const cartPage = pm.onCartPage();
+        const checkoutPage = pm.onCheckoutPage();
+
+        await inventoryPage.addProductToCart(PRODUCT_ONE.name);
+        await inventoryPage.openCart();
+        await cartPage.clickCheckout();
+
+        await checkoutPage.fillShippingInfo("Ivan", "Ivanov", "");
+        await checkoutPage.clickContinue();
+
+        await expect(checkoutPage.getErrorMessage()).toContainText('Postal Code is required');
+    });
+
+    test("Cancel on step one returns to cart and keep items", async ({page}) => {
+        const inventoryPage = pm.onInventoryPage();
+        const cartPage = pm.onCartPage();
+        const checkoutPage = pm.onCheckoutPage();
+
+        await inventoryPage.addProductToCart(PRODUCT_ONE.name);
+        await inventoryPage.openCart();
+        await cartPage.clickCheckout();
+
+        await expect(page).toHaveURL(/.*checkout-step-one.html/);
+        await checkoutPage.clickCancel();
+
+        await expect(page).toHaveURL(/.*cart.html/);
+        expect(await cartPage.getProductName(PRODUCT_ONE.name)).toEqual(PRODUCT_ONE.name);
+    });
+});
