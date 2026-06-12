312ebde8493ac02ef6b65716cb72ee1821c7dca5
danailzahariev <d.zahariev@icloud.com>
2026-06-12 20:53:02 +0300

    Removed God Object PageManager pattern. Implemented custom Playwright fixtures for lazy loading, dependency injection, and cleaner test setup/teardown.

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,22 +1,40 @@
 import {test as base} from "@playwright/test";
-import {PageManager} from "../page-objects/pageManager";
 import {InventoryPage} from "../page-objects/inventory/inventoryPage";
 import {USERS} from "../test-data/testData";
+import {LoginPage} from "../page-objects/auth/loginPage";
+import {CartPage} from "../page-objects/cart/cartPage";
+import {ProductDetailsPage} from "../page-objects/product/productDetailsPage";
+import {CheckoutPage} from "../page-objects/checkout/checkoutPage";
 
 type MyFixtures = {
-    pm: PageManager;
-    authedInventoryPage: InventoryPage;
+    loginPage: LoginPage,
+    authedInventoryPage: InventoryPage,
+    inventoryPage: InventoryPage,
+    cartPage: CartPage,
+    checkoutPage: CheckoutPage,
+    productDetailsPage: ProductDetailsPage;
 };
 
 export const test = base.extend<MyFixtures>({
-    pm: async ({page}, use) => {
-        const pm = new PageManager(page);
-        await use(pm);
+    loginPage: async ({page}, use) => {
+        await use(new LoginPage(page));
     },
+    inventoryPage: async ({page}, use) => {
+        await use(new InventoryPage(page));
+    },
+    cartPage: async ({page}, use) => {
+        await use(new CartPage(page));
+    },
+    productDetailsPage: async ({page}, use) => {
+        await use(new ProductDetailsPage(page));
+    },
+    checkoutPage: async ({page}, use) => {
+        await use(new CheckoutPage(page));
+    },
+    authedInventoryPage: async ({loginPage}, use) => {
+        await loginPage.goto();
+        const inventoryPage = await loginPage.loginSuccess(USERS.STANDARD.username!, USERS.STANDARD.password!);
 
-    authedInventoryPage: async ({pm}, use) => {
-        await pm.loginPage().goto();
-        const inventoryPage = await pm.loginPage().loginSuccess(USERS.STANDARD.username!, USERS.STANDARD.password!);
         await use(inventoryPage);
     },
 });
